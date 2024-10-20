35840f880c3db0d6e9f1360542aaf4540b2c13c4
NotZaxr <mss707d@gmail.com>
2024-10-20 13:59:29 +0300

    logo 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
   return (<nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
         {/* <img className="mx-2 w-10" src={logo} alt="logo"/> */}
-        <h3>Zaxr</h3>
+        <h2 className="text-white">Zaxr</h2>
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
+        <a href=""><FaLinkedin /></a>
         <FaGithub />
         <FaInstagram />
         <FaSquareXTwitter />
